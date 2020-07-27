cb62402a44b5c3a9ed48f3f3f6d322e5668150e9
Diego-EC <diegoezquerro@gmail.com>
2020-07-27 16:07:38 +0000

    exercise finished
    Signed-off-by: Diego-EC <diegoezquerro@gmail.com>

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,43 @@
-/* eslint-disable */
-
-import "../assets/img/rigo-baby.jpg";
-import "../assets/img/4geeks.ico";
-//import 'breathecode-dom'; //DOM override to make JS easier to use
 import "../style/index.scss";
 
 window.onload = function() {
   console.log("Hello Rigo from the console!");
+  generateExcuse();
 };
+
+function generateExcuse() {
+  let randomInt;
+  let who = ["the dog", "my granma", "his turtle", "my bird"];
+  let what = ["eat", "pissed", "crushed", "broked"];
+  let when = [
+    "before the class",
+    "right in time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ];
+  let excuse;
+
+  randomInt = generateRandomInt(0, 3);
+  excuse = who[randomInt];
+  randomInt = generateRandomInt(0, 3);
+  excuse = excuse + " " + what[randomInt];
+  randomInt = generateRandomInt(0, 4);
+  excuse = excuse + " " + when[randomInt];
+
+  document.getElementById("excuse").innerHTML = excuse;
+  //console.log(randomInt);
+}
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive).
+ * The value is no lower than min (or the next integer greater than min
+ * if min isn't an integer) and no greater than max (or the next integer
+ * lower than max if max isn't an integer).
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function generateRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
